bf24570402a060716ca5976880292aa93766683c
ssgushyonka <orangeee04@gmail.com>
2025-01-28 16:10:03 +0300

    Initial Commit

---
+++ TodoListApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TodoListApp
+//
+//  Created by Элина Борисова on 28.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+    }
+
+
+}
+
